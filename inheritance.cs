36646b10954468cae409781594ca527d72f57320
Stephane Vaillancourt <svaillancourt@gmail.com>
2020-05-11 11:10:58 -0600

    added abstraction

---
+++ inheritance.cs
@@ -0,0 +1,30 @@
+﻿// using System;
+
+// namespace inheritance
+// {
+//     class Group
+//     {
+//         public string groupName = "Hikers R Us";
+//         public void greet()
+//         {
+//             Console.WriteLine("Welcome back!");
+//         }
+//     }
+
+//     class User : Group // User inherites from Group
+//     {
+//         public string userName = "jsmith14";
+
+//     }
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             User newUser = new User();
+//             newUser.greet();
+//             Console.WriteLine(newUser.userName + " is a member of " + newUser.groupName);
+
+//         }
+//     }
+// }
+
---
+++ polymorphism.cs
@@ -0,0 +1,43 @@
+﻿// using System;
+
+// namespace polymorphism
+// {
+//     class Program
+//     {
+//         class Animal
+//         {
+//             public virtual void sound()
+//             {
+//                 Console.WriteLine("[Insert Animal Sound]");
+//             }
+//         }
+        
+//         class Pig : Animal 
+//         {
+//             public override void sound()
+//             {
+//                 Console.WriteLine("Oink! Oink!");
+//             }
+//         }
+
+//         class Duck : Animal
+//         {
+//             public override void sound()
+//             {
+//                 Console.WriteLine("Quack! Quack!");
+//             }
+//         }
+        
+//         static void Main(string[] args)
+//         {
+//             Animal myAnimal =  new Animal();
+//             Animal myPig = new Pig();
+//             Animal myDuck = new Duck();
+
+//             myAnimal.sound();
+//             myPig.sound();
+//             myDuck.sound();
+//         }
+//     }
+// }
+
